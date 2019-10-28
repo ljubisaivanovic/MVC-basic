1dbed4dbfcb2ed6756f9c725a1667d66dbd99d13
mr.ivanovic <mr.ivanovic.993@gmail.com>
2019-10-28 16:24:56 +0100

    change style

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -2,18 +2,6 @@
         </div>
         <div class="footer">
             <p>Ljubisa Ivanovic 2019</p>
-
         </div>
-        <style>
-            .footer {
-                position: fixed;
-                left: 0;
-                bottom: 0;
-                width: 100%;
-                background-color: #222;
-                color: white;
-                text-align: center;
-            }
-        </style>
     </body>
 </html>
--- app/views/user/user_login.php
+++ app/views/user/user_login.php
@@ -1,17 +1,35 @@
 <?php require_once("views/layouts/header.php"); ?>
 
+<div class="row ">
+    <div class="col-md-4"></div>
+    <div class="col-md-4 login">
+        <form action="?p=user/login" method="post">
+            <h2 class="text-center">Sign in</h2>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon">Email:</span>
+                    <input type="text" class="form-control" name="email" placeholder="Email" required="required">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon">Password:</span>
+                    <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary login-btn btn-block">Sign in</button>
+            </div>
+            <div class="clearfix">
+                <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
+                <a href="#" class="pull-right">Forgot Password?</a>
+            </div>
+        </form>
+        <p class="text-center text-muted small">Don't have an account? <a href="?p=user/showRegister">Register</a>
 
-
-
-<form action="?p=user/login" method="post">
-
-    <input type="email" name="email" >
-    <input type="password" name="password">
-    <button type="submit">Login</button>
-
-</form>
-You dont have account?
-<a href="?p=user/showRegister">Register</a>
+    </div>
+    <div class="col-md-4"></div>
+</div>
 
 
 <?php require_once("views/layouts/footer.php"); ?>
